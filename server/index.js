f57fd157a75f377285731b1d5ccc067ec1c4d599
Mert Gundogan <mert.gundogan@outlook.com>
2022-12-29 15:43:17 +0300

    initial commit

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const http = require("http").Server(app);
+const PORT = 4000;
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "http://192.168.1.4:3000",
+  },
+});
+
+app.use(cors());
+let users = [];
+
+socketIO.on("connection", (socket) => {
+  console.log(`${socket.id} baglandi`);
+  socket.on("message", (data) => {
+    socketIO.emit("messageResponse", data);
+  });
+
+  socket.on("newUser", (data) => {
+    users.push(data);
+    socketIO.emit("newUserResponse", users);
+  });
+
+  socket.on("disconnect", () => {
+    users = users.filter((user) => user.socketID !== socket.id);
+    socketIO.emit("newUserResponse", users);
+    socket.disconnect();
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
